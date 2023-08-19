77ba06dc5c698a567497f5202042b2f2e13a07c8
devdking <devdkingg@gmail.com>
2023-08-19 14:04:09 -0300

    css and javascipt update

--- script/main.js
+++ script/main.js
@@ -1,27 +1,67 @@
-/* Atribuições */
+/*-------- Atributions -------*/
 let input = document.getElementById("maininput")
 let form = document.getElementById("addtaskform")
-let todoarea = document.getElementById("to-do-area")
+let todoArea = document.getElementById("to-do-area")
 let todo = document.querySelector(".todo")
+let btnArea
 
-/* Funções */
+/*--------- Functions ---------*/
 function create(){
 
     let toDoText = input.value
 
-    let div = document.createElement("div")
-    div.classList.add("todo")
+    if(!toDoText){
+        return alert("Dê um nome para sua tarefa")
+    }
+
+    let divToDo = document.createElement("div")
+    divToDo.classList.add("todo")
 
     let h2 = document.createElement("h2")
     h2.textContent = input.value
-    
-    document.body.appendChild(div)
+    divToDo.appendChild(h2)
 
+    btnArea = document.createElement("div")
+    btnArea.id = "btnArea"
+
+    //done button
+    doneBtn = document.createElement("button")
+    
+    let doneSpan = document.createElement("span")
+    doneSpan.classList.add("material-symbols-outlined")
+    doneSpan.innerText = "done"
+    doneBtn.appendChild(doneSpan)
+    btnArea.appendChild(doneBtn)
+
+    //edit button
+    editBtn = document.createElement("button")
+
+    let editSpan = document.createElement("span")
+    editSpan.classList.add("material-symbols-outlined")
+    editSpan.innerText = "edit"
+    editBtn.appendChild(editSpan)
+    btnArea.appendChild(editBtn)
+
+    //delete button
+    deleteBtn = document.createElement("button")
+
+    let deleteSpan = document.createElement("span")
+    deleteSpan.classList.add("material-symbols-outlined")
+    deleteSpan.innerText = "delete"
+    deleteBtn.appendChild(deleteSpan)
+    btnArea.appendChild(deleteBtn)
+
+    
+    divToDo.appendChild(btnArea)
+    todoArea.appendChild(divToDo)
+
+    input.focus()
+    input.value = ""
 }
 
 
 
-/* Eventos */
+/*----------- Events ----------*/
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 })
