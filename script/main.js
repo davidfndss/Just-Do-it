fa8b6cf09930503dc724f70cf40ea97f0ee45946
devdking <devdkingg@gmail.com>
2023-08-20 02:21:17 -0300

    buttons fixed

--- script/main.js
+++ script/main.js
@@ -10,9 +10,6 @@ function createButtons(divToDo){
 
     
 
-    btnArea = document.createElement("div")
-    btnArea.classList.add = "btnArea"
-
     //done button
     let doneBtn = document.createElement("button")
     
@@ -20,29 +17,29 @@ function createButtons(divToDo){
     doneSpan.classList.add("material-symbols-outlined")
     doneSpan.innerText = "done"
     doneBtn.appendChild(doneSpan)
-    btnArea.appendChild(doneBtn)
+    divToDo.appendChild(doneBtn)
 
-    /*//delete button
+    //delete button
     let deleteBtn = document.createElement("button")
 
     let deleteSpan = document.createElement("span")
     deleteSpan.classList.add("material-symbols-outlined")
     deleteSpan.innerText = "delete"
     deleteBtn.appendChild(deleteSpan)
-    btnArea.appendChild(deleteBtn)
-    deleteBtn.classList.add("hide")*/
+    divToDo.appendChild(deleteBtn)
+    deleteBtn.classList.add("hide")
     
 
     doneBtn.addEventListener("click", function(){
         divToDo.classList.add("done")
-        //doneBtn.classList.add("hide")
-        //deleteBtn.classList.remove("hide")
+        doneBtn.classList.add("hide")
+        deleteBtn.classList.remove("hide")
 
     })
 
-    /*deleteBtn.addEventListener("click", function(){
+    deleteBtn.addEventListener("click", function(){
         divToDo.remove()
-    })*/
+    })
 
 }
 
@@ -63,7 +60,6 @@ function create(){
 
     createButtons(divToDo)
 
-    divToDo.appendChild(btnArea)
 
     todoArea.appendChild(divToDo)
 
