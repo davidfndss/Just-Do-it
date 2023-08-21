711ab6c3da5ae8923daacc46c0c853530f791dae
DevKing <devdkingg@gmail.com>
2023-08-21 17:58:14 -0300

    update

--- script/edit.js
+++ script/edit.js
@@ -1,7 +1,23 @@
-    //let doneBtn = document.createElement("button")
+ let mainEditBtn = document.getElementById("editbtn")
+
+ let editBtn = document.createElement("button")
         
-        //let doneSpan = document.createElement("span")
-        //doneSpan.classList.adD("material-symbols-outlined")
-        //doneSpan.innerText = "done"
-        //doneBtn.appendChild(doneSpan)
-        //divToDo.appendChild(doneBtn)
+        let editSpan = document.createElement("span")
+        editSpan.classList.add("material-symbols-outlined")
+        editSpan.innerText = "edit"
+        editBtn.appendChild(editSpan)
+
+function receiveButtons(){
+
+    let todos = document.querySelectorAll(".todo")
+    todos.appendChild(editBtn)
+    
+}
+
+
+// Events-----------
+
+mainEditBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+})
+
